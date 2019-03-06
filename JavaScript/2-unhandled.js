84457b9c9c91bd6f2282308447d78f530e9bddc5
tshemsedinov <timur.shemsedinov@gmail.com>
2019-03-06 23:14:01 +0200

    Handled and unhandled

---
+++ JavaScript/2-unhandled.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const sum = (a, b) => new Promise((resolve, reject) => {
+  if (typeof a === 'number' && typeof b === 'number') {
+    resolve(a + b);
+  } else {
+    reject(new Error('a and b should be numbers'));
+  }
+});
+
+sum(7, 'A')
+  .then(data => {
+    console.log(data);
+  });
+
+// UnhandledPromiseRejectionWarning: Error: a and b should be numbers
