aec3b1792549b93510546670294f802ad594d459
tshemsedinov <timur.shemsedinov@gmail.com>
2019-03-07 14:32:14 +0200

    Add multiple resolve example

---
+++ JavaScript/8-multiple.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const sum = (a, b) => new Promise((resolve, reject) => {
+  if (typeof a === 'number' && typeof b === 'number') {
+    resolve(a + b);
+    resolve(a + b);
+  } else {
+    reject(new Error('a and b should be numbers'));
+  }
+});
+
+process.on('multipleResolves', (type, promise, reason) => {
+  console.log({ multipleResolves: { type, promise, reason } });
+});
+
+sum(7, 3)
+  .then(res1 => {
+    console.log({ res1 });
+  });
