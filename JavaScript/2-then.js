976bd897715d95e02b042dd6288cbf7029d152e7
tshemsedinov <timur.shemsedinov@gmail.com>
2019-03-07 13:27:21 +0200

    Add .then and .finally examples

---
+++ JavaScript/2-then.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const sum = (a, b) => new Promise((resolve, reject) => {
+  if (typeof a === 'number' && typeof b === 'number') {
+    resolve(a + b);
+  } else {
+    reject(new Error('a and b should be numbers'));
+  }
+});
+
+sum(7, 'A')
+  .then(
+    data => {
+      console.log({ data });
+    },
+    err => {
+      console.log({ messageThen: err.message });
+    }
+  )
+  .catch(err => {
+    console.log({ messageCatch: err.message });
+  });
---
+++ JavaScript/3-finally.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const sum = (a, b) => new Promise((resolve, reject) => {
+  if (typeof a === 'number' && typeof b === 'number') {
+    resolve(a + b);
+  } else {
+    reject(new Error('a and b should be numbers'));
+  }
+});
+
+sum(7, 'A')
+  .then(data => {
+    console.log({ data });
+  })
+  .catch(err => {
+    console.log({ messageCatch: err.message });
+  })
+  .finally(() => {
+    console.log('finally');
+  });
